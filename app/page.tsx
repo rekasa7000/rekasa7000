4451a5f4413fe2606327952c7195114a37225373
Regee <regeecasana57@gmail.com>
2024-10-04 21:14:19 +0800

    initial

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Regee</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
