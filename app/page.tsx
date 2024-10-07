3119383cee040fbda07b7fbbca51ea10de201221
Regee <regeecasana57@gmail.com>
2024-10-07 21:21:42 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,158 @@
 "use client";
 import Navigation from "@/components/Navigation";
 import SocialMedia from "@/components/SocialMedia";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [activeSection, setActiveSection] = useState("#about");
-
-  useEffect(() => {
-    setActiveSection("#about");
-  }, []);
-
-  const handleSetActive = (section: string) => {
-    setActiveSection(section);
-  };
   return (
-    <div className="flex justify-center items-center h-dvh w-full container max-w-6xl m-auto">
-      <main className="pt-24 lg:w-1/2 lg:py-24" id="content">
-        <section
-          id="about"
-          className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
-        >
-          <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
-            <h2 className="text-sm font-bold uppercase tracking-widest  lg:sr-only">
-              About me
+    <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+      <div className="lg:flex lg:flex-row-reverse lg:justify-between lg:gap-4 container">
+        <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24 text-end">
+          <div>
+            <h1 className="text-4xl font-bold tracking-tight text-slate-900 sm:text-5xl">
+              <a href="/">Regee Casaña</a>
+            </h1>
+            <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-700 sm:text-xl">
+              Full Stack & Software Engineer
             </h2>
+            <p className="mt-4 leading-normal">
+              I build websites and software to the best of my abilities and
+              knowledge.
+            </p>
+            <Navigation />
           </div>
-        </section>
-      </main>
-      <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-end lg:py-24 text-end">
-        <h1 className="text-4xl font-bold tracking-tight text-slate-900 sm:text-5xl">
-          <a href="/">Regee Casaña</a>
-        </h1>
-        <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-700 sm:text-xl">
-          Full Stack & Software Engineer
-        </h2>
-        <p className="mt-4 leading-normal">
-          I build websites and software to the best of my abilities and
-          knowledge.
-        </p>
-        <Navigation />
-        <SocialMedia />
-      </header>
+
+          <SocialMedia />
+        </header>
+        <main className="pt-24 lg:w-1/2 lg:py-24">
+          <section
+            id="about"
+            className="about-section mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+          >
+            <p>
+              I am a dedicated full-stack developer with a deep passion for
+              creating efficient, user-friendly websites and software. My
+              journey into tech began with a fascination for problem-solving and
+              has since evolved into a full-time commitment to building
+              impactful digital solutions. Whether it’s developing complex
+              applications or refining user interfaces, I always strive to
+              deliver work that blends both functionality and aesthetics.
+            </p>
+            <p>
+              Technology is ever-changing, and I believe in constantly evolving
+              along with it. I invest time in learning new tools, frameworks,
+              and techniques to stay ahead in the field. This commitment has
+              allowed me to work on a variety of projects, each expanding my
+              knowledge in web development, software engineering, and cloud
+              technologies. I value challenges as they push me to innovate and
+              find creative solutions to problems.
+            </p>
+            <p>
+              Collaboration is at the heart of my work. I love working closely
+              with clients and teams to bring ideas to life. Understanding the
+              user’s needs and translating them into intuitive digital
+              experiences is what drives me. My focus is always on delivering
+              high-quality products that leave a lasting impact and serve the
+              needs of the business and the end-users alike.
+            </p>
+          </section>
+          <section
+            id="experience"
+            className="about-section mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+          >
+            <h2>Experience</h2>
+            <p>
+              I am a dedicated full-stack developer with a deep passion for
+              creating efficient, user-friendly websites and software. My
+              journey into tech began with a fascination for problem-solving and
+              has since evolved into a full-time commitment to building
+              impactful digital solutions. Whether it’s developing complex
+              applications or refining user interfaces, I always strive to
+              deliver work that blends both functionality and aesthetics.
+            </p>
+            <p>
+              Technology is ever-changing, and I believe in constantly evolving
+              along with it. I invest time in learning new tools, frameworks,
+              and techniques to stay ahead in the field. This commitment has
+              allowed me to work on a variety of projects, each expanding my
+              knowledge in web development, software engineering, and cloud
+              technologies. I value challenges as they push me to innovate and
+              find creative solutions to problems.
+            </p>
+            <p>
+              Collaboration is at the heart of my work. I love working closely
+              with clients and teams to bring ideas to life. Understanding the
+              user’s needs and translating them into intuitive digital
+              experiences is what drives me. My focus is always on delivering
+              high-quality products that leave a lasting impact and serve the
+              needs of the business and the end-users alike.
+            </p>
+          </section>
+          <section
+            id="projects"
+            className="about-section mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+          >
+            <h2>Projects</h2>
+            <p>
+              I am a dedicated full-stack developer with a deep passion for
+              creating efficient, user-friendly websites and software. My
+              journey into tech began with a fascination for problem-solving and
+              has since evolved into a full-time commitment to building
+              impactful digital solutions. Whether it’s developing complex
+              applications or refining user interfaces, I always strive to
+              deliver work that blends both functionality and aesthetics.
+            </p>
+            <p>
+              Technology is ever-changing, and I believe in constantly evolving
+              along with it. I invest time in learning new tools, frameworks,
+              and techniques to stay ahead in the field. This commitment has
+              allowed me to work on a variety of projects, each expanding my
+              knowledge in web development, software engineering, and cloud
+              technologies. I value challenges as they push me to innovate and
+              find creative solutions to problems.
+            </p>
+            <p>
+              Collaboration is at the heart of my work. I love working closely
+              with clients and teams to bring ideas to life. Understanding the
+              user’s needs and translating them into intuitive digital
+              experiences is what drives me. My focus is always on delivering
+              high-quality products that leave a lasting impact and serve the
+              needs of the business and the end-users alike.
+            </p>
+          </section>
+          <section
+            id="education"
+            className="about-section mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+          >
+            <h2>Education</h2>
+            <p>
+              I am a dedicated full-stack developer with a deep passion for
+              creating efficient, user-friendly websites and software. My
+              journey into tech began with a fascination for problem-solving and
+              has since evolved into a full-time commitment to building
+              impactful digital solutions. Whether it’s developing complex
+              applications or refining user interfaces, I always strive to
+              deliver work that blends both functionality and aesthetics.
+            </p>
+            <p>
+              Technology is ever-changing, and I believe in constantly evolving
+              along with it. I invest time in learning new tools, frameworks,
+              and techniques to stay ahead in the field. This commitment has
+              allowed me to work on a variety of projects, each expanding my
+              knowledge in web development, software engineering, and cloud
+              technologies. I value challenges as they push me to innovate and
+              find creative solutions to problems.
+            </p>
+            <p>
+              Collaboration is at the heart of my work. I love working closely
+              with clients and teams to bring ideas to life. Understanding the
+              user’s needs and translating them into intuitive digital
+              experiences is what drives me. My focus is always on delivering
+              high-quality products that leave a lasting impact and serve the
+              needs of the business and the end-users alike.
+            </p>
+          </section>
+        </main>
+      </div>
     </div>
   );
 }
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,34 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Navigation = () => {
+  const [activeSection, setActiveSection] = useState("#about");
+
+  const handleSetActive = (sectionId: string) => {
+    setActiveSection(sectionId);
+  };
+
   return (
     <nav
       className="nav hidden lg:flex justify-end"
       aria-label="In-page jump links"
     >
-      <ul className="mt-16 w-max ">
+      <ul className="mt-16 w-max">
         <li>
-          <a className="group flex items-center py-3 active" href="#about">
-            <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-600 group-hover:text-slate-400 group-focus-visible:text-slate-200">
+          <a
+            className={`group flex items-center py-3 ${
+              activeSection === "#about" ? "active" : ""
+            }`}
+            href="#about"
+            onClick={() => handleSetActive("#about")}
+          >
+            <span className="text-xs w-full font-bold uppercase tracking-widest text-slate-600 group-hover:text-slate-400 group-focus-visible:text-slate-200">
               About
             </span>
-            <span className="nav-indicator ml-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-400 group-focus-visible:w-16 group-focus-visible:bg-slate-600 motion-reduce:transition-none"></span>
           </a>
         </li>
         <li>
-          <a className="group flex items-center py-3 active" href="#experience">
-            <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-600 group-hover:text-slate-400 group-focus-visible:text-slate-200">
+          <a
+            className={`group flex items-center py-3 ${
+              activeSection === "#experience" ? "active" : ""
+            }`}
+            href="#experience"
+            onClick={() => handleSetActive("#experience")}
+          >
+            <span className="text-xs w-full font-bold uppercase tracking-widest text-slate-600 group-hover:text-slate-400 group-focus-visible:text-slate-200">
               Experience
             </span>
-            <span className="nav-indicator ml-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-400 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
           </a>
         </li>
         <li>
-          <a className="group flex items-center py-3 active" href="#projects">
-            <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-600 group-hover:text-slate-400 group-focus-visible:text-slate-200">
+          <a
+            className={`group flex items-center py-3 ${
+              activeSection === "#projects" ? "active" : ""
+            }`}
+            href="#projects"
+            onClick={() => handleSetActive("#projects")}
+          >
+            <span className="text-xs w-full font-bold uppercase tracking-widest text-slate-600 group-hover:text-slate-400 group-focus-visible:text-slate-200">
               Projects
             </span>
-            <span className="nav-indicator ml-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-400 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+          </a>
+        </li>
+        <li>
+          <a
+            className={`group flex items-center py-3 ${
+              activeSection === "#projects" ? "active" : ""
+            }`}
+            href="#education"
+            onClick={() => handleSetActive("#education")}
+          >
+            <span className="text-xs w-full font-bold uppercase tracking-widest text-slate-600 group-hover:text-slate-400 group-focus-visible:text-slate-200">
+              Education
+            </span>
           </a>
         </li>
       </ul>
