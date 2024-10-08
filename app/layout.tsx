4219a8821f8f8adf19c29916d968beecf4c90fea
Regee <regeecasana57@gmail.com>
2024-10-08 20:10:41 +0800

    added about me

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import { Inter } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+import "./globals.css";
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -25,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased leading-relaxed`}
-      >
+      <body className={`${inter.className} antialiased leading-relaxed`}>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Navigation from "@/components/Navigation";
 import SocialMedia from "@/components/SocialMedia";
+import { Card, CardContent } from "@/components/ui/card";
 
 export default function Home() {
   return (
@@ -26,131 +27,126 @@ export default function Home() {
         <main className="pt-24 lg:w-1/2 lg:py-24">
           <section
             id="about"
-            className="about-section mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+            className="about-section  scroll-mt-16 md:mb-24 lg:mb-12 lg:scroll-mt-24 lg:text-pretty text-wrap font-medium text-lg"
           >
             <p>
-              I am a dedicated full-stack developer with a deep passion for
-              creating efficient, user-friendly websites and software. My
-              journey into tech began with a fascination for problem-solving and
-              has since evolved into a full-time commitment to building
-              impactful digital solutions. Whether it’s developing complex
-              applications or refining user interfaces, I always strive to
-              deliver work that blends both functionality and aesthetics.
+              Hello there! I’m a passionate software engineer who believes in
+              freedom, growth, and doing everything with purpose. I currently
+              work at a{" "}
+              <a
+                href="https://www.kloudtechsea.com"
+                className="font-bold"
+                target="_blank"
+              >
+                startup company{" "}
+              </a>{" "}
+              where I manage both the server-side and client-side development,
+              along with database management and DevOps.
             </p>
             <p>
-              Technology is ever-changing, and I believe in constantly evolving
-              along with it. I invest time in learning new tools, frameworks,
-              and techniques to stay ahead in the field. This commitment has
-              allowed me to work on a variety of projects, each expanding my
-              knowledge in web development, software engineering, and cloud
-              technologies. I value challenges as they push me to innovate and
-              find creative solutions to problems.
+              I created and built softwares like a{" "}
+              <a
+                href="https://www.github.com/rekasa7000/databox"
+                target="_blank"
+                className="font-bold"
+              >
+                scheduling system{" "}
+              </a>
+              and web-based applications like an{" "}
+              <a
+                href="https://www.github.com/rekasa7000/knowtproject"
+                target="_blank"
+                className="font-bold"
+              >
+                article summarizer{" "}
+              </a>
+              and a{" "}
+              <a
+                href="https://github.com/dadeyyy/CultureConnectResearch.git"
+                target="_blank"
+                className="font-bold"
+              >
+                social media.{" "}
+              </a>{" "}
+              I’m always open to collaboration as it really helps me to learn
+              more and expand my knowledge. I enjoy learning into deep
+              algorithms and data structures, and I love reading documentation
+              to stay updated and improve my skills.
             </p>
             <p>
-              Collaboration is at the heart of my work. I love working closely
-              with clients and teams to bring ideas to life. Understanding the
-              user’s needs and translating them into intuitive digital
-              experiences is what drives me. My focus is always on delivering
-              high-quality products that leave a lasting impact and serve the
-              needs of the business and the end-users alike.
+              When I’m not coding, I enjoy reading books, listening to music,
+              and playing Dota 2. I also like a particular workout called
+              pull-ups. After doing all of the above, I finally cuddle with my
+              cat, named Miming.
             </p>
           </section>
           <section
             id="experience"
-            className="about-section mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+            className="about-section mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24 flex flex-col gap-3"
           >
-            <h2>Experience</h2>
-            <p>
-              I am a dedicated full-stack developer with a deep passion for
-              creating efficient, user-friendly websites and software. My
-              journey into tech began with a fascination for problem-solving and
-              has since evolved into a full-time commitment to building
-              impactful digital solutions. Whether it’s developing complex
-              applications or refining user interfaces, I always strive to
-              deliver work that blends both functionality and aesthetics.
-            </p>
-            <p>
-              Technology is ever-changing, and I believe in constantly evolving
-              along with it. I invest time in learning new tools, frameworks,
-              and techniques to stay ahead in the field. This commitment has
-              allowed me to work on a variety of projects, each expanding my
-              knowledge in web development, software engineering, and cloud
-              technologies. I value challenges as they push me to innovate and
-              find creative solutions to problems.
-            </p>
-            <p>
-              Collaboration is at the heart of my work. I love working closely
-              with clients and teams to bring ideas to life. Understanding the
-              user’s needs and translating them into intuitive digital
-              experiences is what drives me. My focus is always on delivering
-              high-quality products that leave a lasting impact and serve the
-              needs of the business and the end-users alike.
-            </p>
+            <Card>
+              <CardContent className="grid grid-cols-6 gap-2 py-3">
+                <div className="w-full col-span-2 text-sm">2024 — Present</div>
+                <div className="w-full col-span-4">
+                  <div className="flex items-center gap-2">
+                    <span className="font-bold">
+                      Full Stack Developer • Kloudtech
+                    </span>
+                    <svg
+                      width="15"
+                      height="15"
+                      viewBox="0 0 15 15"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z"
+                        fill="currentColor"
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardContent className="grid grid-cols-6 gap-2 py-3">
+                <div className="w-full col-span-2 text-sm text-center">
+                  2023 — 2024
+                </div>
+                <div className="w-full col-span-4">
+                  <div className="flex items-center gap-2">
+                    <span className="font-bold">
+                      Backend Developer Intern • Kloudtech
+                    </span>
+                    <svg
+                      width="15"
+                      height="15"
+                      viewBox="0 0 15 15"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z"
+                        fill="currentColor"
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
           </section>
           <section
             id="projects"
             className="about-section mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
-          >
-            <h2>Projects</h2>
-            <p>
-              I am a dedicated full-stack developer with a deep passion for
-              creating efficient, user-friendly websites and software. My
-              journey into tech began with a fascination for problem-solving and
-              has since evolved into a full-time commitment to building
-              impactful digital solutions. Whether it’s developing complex
-              applications or refining user interfaces, I always strive to
-              deliver work that blends both functionality and aesthetics.
-            </p>
-            <p>
-              Technology is ever-changing, and I believe in constantly evolving
-              along with it. I invest time in learning new tools, frameworks,
-              and techniques to stay ahead in the field. This commitment has
-              allowed me to work on a variety of projects, each expanding my
-              knowledge in web development, software engineering, and cloud
-              technologies. I value challenges as they push me to innovate and
-              find creative solutions to problems.
-            </p>
-            <p>
-              Collaboration is at the heart of my work. I love working closely
-              with clients and teams to bring ideas to life. Understanding the
-              user’s needs and translating them into intuitive digital
-              experiences is what drives me. My focus is always on delivering
-              high-quality products that leave a lasting impact and serve the
-              needs of the business and the end-users alike.
-            </p>
-          </section>
+          ></section>
           <section
             id="education"
             className="about-section mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
-          >
-            <h2>Education</h2>
-            <p>
-              I am a dedicated full-stack developer with a deep passion for
-              creating efficient, user-friendly websites and software. My
-              journey into tech began with a fascination for problem-solving and
-              has since evolved into a full-time commitment to building
-              impactful digital solutions. Whether it’s developing complex
-              applications or refining user interfaces, I always strive to
-              deliver work that blends both functionality and aesthetics.
-            </p>
-            <p>
-              Technology is ever-changing, and I believe in constantly evolving
-              along with it. I invest time in learning new tools, frameworks,
-              and techniques to stay ahead in the field. This commitment has
-              allowed me to work on a variety of projects, each expanding my
-              knowledge in web development, software engineering, and cloud
-              technologies. I value challenges as they push me to innovate and
-              find creative solutions to problems.
-            </p>
-            <p>
-              Collaboration is at the heart of my work. I love working closely
-              with clients and teams to bring ideas to life. Understanding the
-              user’s needs and translating them into intuitive digital
-              experiences is what drives me. My focus is always on delivering
-              high-quality products that leave a lasting impact and serve the
-              needs of the business and the end-users alike.
-            </p>
-          </section>
+          ></section>
         </main>
       </div>
     </div>
