1454cc7c6e81f80da2a3b88e4bae9dbfc7458590
rekasa7000 <regeecasana57@gmail.com>
2025-01-07 15:52:59 +0800

    regee

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
 "use client";
 export default function Home() {
-  return <div className="text-[500rem] bg-ceremonial">REGEE CASAÑA</div>;
+  return (
+    <div className="text-[500rem] my-0 py-0 bg-white w-full leading-[0.15] h-screen text-ceremonial text-nowrap overflow-x-auto">
+      REGEE CASAÑA
+    </div>
+  );
 }
