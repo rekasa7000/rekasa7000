7ba9ef6e72f41ae13c7b3cb1012f8579d3bb144f
rekasa7000 <regeecasana57@gmail.com>
2025-09-23 01:16:04 +0800

    removed @nuxt/eslint from the modules in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
 
-  modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon", "@nuxt/image", "@nuxt/ui"],
+  modules: ["@nuxt/fonts", "@nuxt/icon", "@nuxt/image", "@nuxt/ui"],
 
   nitro: {
     esbuild: {
